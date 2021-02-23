268e81857b107ce831cf9ff300a6aa1a89200deb
foxamanda2 <fox.amanda2@gmail.com>
2021-02-23 14:46:36 -0500

    Selector working. Randomizer Button not working yet.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+function ColorPickerStyle() {
+  const randomizerHue = Math.floor(Math.random() * 360)
+  const randomizerSL = Math.floor(Math.random() * 100)
+
+  const [hue, setHue] = useState(randomizerHue)
+  const [sat, setSat] = useState(randomizerSL)
+  const [light, setLight] = useState(randomizerSL)
+
+  return (
+    <div>
+      <h1>Color Picker</h1>
+
+      <div
+        className="ColorBox"
+        style={{
+          backgroundColor: `hsl(${hue},${sat}%,${light}%)`,
+        }}
+      ></div>
+      <div>
+        <input
+          className="hue-slider"
+          type="range"
+          min="0"
+          max="360"
+          value={hue}
+          onChange={function (event) {
+            setHue(event.target.value)
+          }}
+        />
+        <div className="hue">Hue:{hue}</div>
+
+        <input
+          className="saturation-slider"
+          type="range"
+          min="0"
+          max="100"
+          value={sat}
+          onChange={function (event) {
+            setSat(event.target.value)
+          }}
+        />
+        <div className="saturation">Saturation:{sat}</div>
+
+        <input
+          className="light-slider"
+          type="range"
+          min="0"
+          max="100"
+          value={light}
+          onChange={function (event) {
+            setLight(event.target.value)
+          }}
+        />
+        <div className="light">Light:{light}</div>
+      </div>
+
+      <button
+        onClick={function () {
+          setHue(hue)
+        }}
+      >
+        Randomize
+      </button>
+    </div>
+  )
+}
 
 export function App() {
-  return <div>Hello, World!</div>
+  return <ColorPickerStyle />
 }
